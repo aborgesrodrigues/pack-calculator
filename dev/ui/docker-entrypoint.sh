c55dbb05d9856e06afd8fcd768331a0931ad3e79
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2026-05-20 22:11:20 -0300

    fix ui deployment at railway

--- dev/ui/docker-entrypoint.sh
+++ dev/ui/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
-export API_UPSTREAM="${API_UPSTREAM:-app:8080}"
-export NGINX_RESOLVER="${NGINX_RESOLVER:-127.0.0.11 valid=10s}"
+# Railway: private DNS + api service name. Local Compose: Docker service "app".
+if [ -n "${RAILWAY_ENVIRONMENT:-}" ] || [ -n "${RAILWAY_SERVICE_ID:-}" ]; then
+  export API_UPSTREAM="${API_UPSTREAM:-api.railway.internal:8080}"
+  export NGINX_RESOLVER="${NGINX_RESOLVER:-[fd12::10] ipv6=on valid=1s}"
+else
+  export API_UPSTREAM="${API_UPSTREAM:-app:8080}"
+  export NGINX_RESOLVER="${NGINX_RESOLVER:-127.0.0.11 valid=10s}"
+fi
 
 envsubst '${API_UPSTREAM} ${NGINX_RESOLVER}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
+
+nginx -t
 
 exec nginx -g 'daemon off;'
